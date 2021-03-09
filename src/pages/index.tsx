496f11e516f2e0dd19cbf6b006ac12b5c4e33fa4
nbaosullivan <nbaosullivan@gmail.com>
2021-03-09 08:39:34 +0000

    style: update holding page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, PageProps } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage: React.FC<PageProps> = () => (
   <Layout>
     <SEO title="Home" />
     {/*<Section>*/}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.13em',
+    },
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s linear infinite',
+      }
+    }
   },
   variants: {
     extend: {},
